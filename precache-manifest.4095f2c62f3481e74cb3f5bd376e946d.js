c68bcbd70a66859e4aef6e7e0a7abc1abe34be1f
Yeongmin Kim <oinochoe@naver.com>
2020-02-22 14:23:10 +0900

    Updates

---
+++ precache-manifest.4095f2c62f3481e74cb3f5bd376e946d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c142d750f17f083f82be246a001aed",
+    "url": "/News_Feed/index.html"
+  },
+  {
+    "revision": "0689f0db692b474576f5",
+    "url": "/News_Feed/static/js/2.31bcdf69.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/News_Feed/static/js/2.31bcdf69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4645eaf8454e62c9ae8a",
+    "url": "/News_Feed/static/js/main.3b3a4bcd.chunk.js"
+  },
+  {
+    "revision": "4ab0f76219de2f1d0d88",
+    "url": "/News_Feed/static/js/runtime-main.cbf59311.js"
+  }
+]);
