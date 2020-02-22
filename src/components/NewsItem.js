08c0ea316d8df37a3b6ccaaccb9626fd6344ddae
Yeongmin Kim <oinochoe@naver.com>
2020-02-22 14:19:32 +0900

    news-feed 분리

---
+++ src/components/NewsItem.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NewsItemBlock = styled.div`
+    display: flex;
+    @media screen and (max-width: 768px) {
+        width: 100%;
+        display: block;
+    }
+    .thumbnail {
+        margin-right: 1rem;
+        img {
+            display: block;
+            width: 160px;
+            height: 100px;
+            object-fit: cover;
+            @media screen and (max-width: 768px) {
+                width: 100%;
+                margin-bottom: 0.5rem;
+            }
+        }
+    }
+    .contents {
+        h2 {
+            margin: 0;
+            a {
+                color: black;
+            }
+        }
+        p {
+            margin: 0;
+            line-height: 1.5;
+            margin-top: 0.5rem;
+            white-space: normal;
+        }
+    }
+    & + & {
+        margin-top: 2rem;
+    }
+`;
+
+const NewsItem = ({ article }) => {
+    const { title, description, url, urlToImage } = article;
+    return (
+        <NewsItemBlock>
+            {urlToImage && (
+                <div className="thumbnail">
+                    <a href={url} target="_blank" rel="noopener noreferrer">
+                        <img src={urlToImage} alt="thumbnail" />
+                    </a>
+                </div>
+            )}
+            <div className="contents">
+                <h2>
+                    <a href={url} target="_blank" rel="noopener noreferrer">
+                        {title}
+                    </a>
+                </h2>
+                <p>{description}</p>
+            </div>
+        </NewsItemBlock>
+    );
+};
+
+export default NewsItem;
